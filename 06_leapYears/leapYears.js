eab83dafd375eaf576f2bfd211b592e68682c93c
Martin <Martin@Jarneid.no>
2022-02-25 10:36:12 +0100

    Add solved exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let isLeapYear;
+    if (year % 400 === 0) isLeapYear = true;
+    else if (year % 100 === 0) isLeapYear = false;
+    else if (year % 4 === 0) isLeapYear = true;
+    else isLeapYear = false;
+    return isLeapYear;
 };
 
 // Do not edit below this line
