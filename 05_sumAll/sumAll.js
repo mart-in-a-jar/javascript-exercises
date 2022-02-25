bfee72028988c39eddda302eb809b05733d195a0
Martin <Martin@Jarneid.no>
2022-02-25 10:28:43 +0100

    Add solved exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+    if (n1 < 0 || n2 < 0 || !Number.isInteger(n1) || !Number.isInteger(n2)) return "ERROR";
+    let sum = 0;
+    let lowNum, highNum;
+    if (n1 < n2) {
+        lowNum = n1;
+        highNum = n2;
+    } else {
+        lowNum = n2;
+        highNum = n1;
+    }
+    for (let i = lowNum; i <= highNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
