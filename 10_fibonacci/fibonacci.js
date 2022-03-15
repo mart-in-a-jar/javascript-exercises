cc410d26003e8a8a5720f27bddeba33d98348a14
Martin <Martin@Jarneid.no>
2022-03-15 14:06:21 +0100

    Add solved exercises 10-12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 1) return "OOPS";
+    fib = [1, 1];
+    for(let i = 1; i < num - 1; i++) {
+        fib.push(fib[i] + fib[i - 1]);
+    }
+    return fib[num - 1];
 };
+
+// 1 1 2 3 5 8
 
 // Do not edit below this line
 module.exports = fibonacci;
