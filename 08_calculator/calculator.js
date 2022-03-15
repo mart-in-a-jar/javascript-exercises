44e592b52bd8ccfd948250cf3598c8f4ec4c71bb
Martin <Martin@Jarneid.no>
2022-03-15 13:25:59 +0100

    Add solved exercise 8 and 9

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, num) => (total + num), 0);
 };
 
-const multiply = function() {
 
+const multiply = function(array) {
+  return array.reduce((total, num) => (total * num), 1); //gir 1 hvis tomt array
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let sum = 1;
+	for(let i = num; i > 0; i--) {
+    sum *= i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
