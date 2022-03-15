4f41e9139bfdb176bc45a749883474edfca5d464
Martin <Martin@Jarneid.no>
2022-03-15 14:05:41 +0100

    Add solved exercises 10-12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const currentYear = new Date().getFullYear();
 
+const findTheOldest = function(array) {
+    array.sort((a, b) => {
+        if (!a.yearOfDeath) a.yearOfDeath = currentYear;
+        if (!b.yearOfDeath) b.yearOfDeath = currentYear;
+        return a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth ? -1 : 1;
+    });
+    return array[0];
+};
+
+const findTheOldestWithReduce = function(array) {
+    return array.reduce((a, b) => {
+        if (!a.yearOfDeath) a.yearOfDeath = currentYear;
+        if (!b.yearOfDeath) b.yearOfDeath = currentYear;
+        return a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth ? a : b;
+    });
 };
 
 // Do not edit below this line
